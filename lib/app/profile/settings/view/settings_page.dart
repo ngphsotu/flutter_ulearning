f1ac2ddb22df1d0fd9d2d7481f9c1bd9646c686d
T <t@Ts-Mac-mini.local>
2023-05-28 02:37:40 +0700

    Clean code

--- lib/app/profile/settings/view/settings_page.dart
+++ lib/app/profile/settings/view/settings_page.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 import '/global.dart';
 import '../settings.dart';
@@ -26,46 +25,7 @@ class _SettingsPageState extends State<SettingsPage> {
             return Container(
               child: Column(
                 children: [
-                  GestureDetector(
-                    onTap: () {
-                      if (kDebugMode) {
-                        print('Tapped Logout');
-                      }
-                      showDialog(
-                        context: context,
-                        builder: (context) {
-                          return AlertDialog(
-                            title: const ReusableText(text: 'Confirm logout'),
-                            content: const ReusableText(text: 'Are you sure ?'),
-                            actions: [
-                              TextButton(
-                                onPressed: () {
-                                  Navigator.of(context).pop();
-                                  if (kDebugMode) {
-                                    print('Pressed Cancel Logout Text Button');
-                                  }
-                                },
-                                child: const ReusableText(text: 'Cancel'),
-                              ),
-                              TextButton(
-                                onPressed: _removeUserData,
-                                child: const ReusableText(text: 'Confirm'),
-                              ),
-                            ],
-                          );
-                        },
-                      );
-                    },
-                    child: Container(
-                      height: 100.h,
-                      decoration: const BoxDecoration(
-                        image: DecorationImage(
-                          fit: BoxFit.fitHeight,
-                          image: AssetImage('assets/icons/logout.png'),
-                        ),
-                      ),
-                    ),
-                  ),
+                  settingsButton(context, _removeUserData),
                 ],
               ),
             );
--- lib/app/profile/settings/widgets/settings_widgets.dart
+++ lib/app/profile/settings/widgets/settings_widgets.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
@@ -15,6 +16,50 @@ AppBar buildAppBar() {
           color: AppColors.primaryText,
           fontSize: 16.sp,
           fontWeight: FontWeight.bold,
+        ),
+      ),
+    ),
+  );
+}
+
+// * Build Settings Button Widget in Settings Page
+Widget settingsButton(context, Function()? function) {
+  return GestureDetector(
+    onTap: () {
+      if (kDebugMode) {
+        print('Tapped Logout');
+      }
+      showDialog(
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            title: const ReusableText(text: 'Confirm logout'),
+            content: const ReusableText(text: 'Are you sure ?'),
+            actions: [
+              TextButton(
+                onPressed: () {
+                  Navigator.of(context).pop();
+                  if (kDebugMode) {
+                    print('Pressed Cancel Logout Text Button');
+                  }
+                },
+                child: const ReusableText(text: 'Cancel'),
+              ),
+              TextButton(
+                onPressed: function,
+                child: const ReusableText(text: 'Confirm'),
+              ),
+            ],
+          );
+        },
+      );
+    },
+    child: Container(
+      height: 100.h,
+      decoration: const BoxDecoration(
+        image: DecorationImage(
+          fit: BoxFit.fitHeight,
+          image: AssetImage('assets/icons/logout.png'),
         ),
       ),
     ),
